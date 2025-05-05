0c2835cff1b2a156d2cbd17f57dc74744fc674d1
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2025-05-05 14:39:01 -0500

    recommit bootstrap

---
+++ bootstrap-project2/script.js
@@ -0,0 +1,13 @@
+<script>
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+<<<<<<< HEAD
+
+</script>
+
+=======
+</script>
+>>>>>>> aef1f4178455742ed588a055b375426f1bf23782
